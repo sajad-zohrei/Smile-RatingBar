8ecd92f30107cae3916be2dbcf873ab1d03d6805
sajad <sajad.zohrei@yahoo.com>
2020-12-22 23:06:52 +0330

    create library

--- app/src/main/java/ir/programmerhive/app/smileratingbar/MainActivity.java
+++ app/src/main/java/ir/programmerhive/app/smileratingbar/MainActivity.java
@@ -3,6 +3,11 @@ package ir.programmerhive.app.smileratingbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+
+import ir.programmerhive.lib.SmileRatingBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txtRatingBarState = findViewById(R.id.txtRatingBarState);
+        SmileRatingBar smileRatingBar = findViewById(R.id.smileRatingBar);
+
+        txtRatingBarState.setText(MessageFormat.format("Rating : {0}", smileRatingBar.getRating()));
+
+        smileRatingBar.setOnRatingBarChangeListener((ratingBar, rate, b) -> {
+            txtRatingBarState.setText(MessageFormat.format("Rating : {0}", rate));
+        });
+
     }
 }
---
+++ lib/src/main/java/ir/programmerhive/lib/SmileRatingBar.java
@@ -0,0 +1,117 @@
+package ir.programmerhive.lib;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatRatingBar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+
+/**
+ * Created by mannb on 7/24/2017.
+ */
+
+public class SmileRatingBar extends AppCompatRatingBar {
+
+    private int[] iconArrayActive = {
+            R.drawable.ic_sentiment_very_dissatisfied,
+            R.drawable.ic_sentiment_dissatisfied,
+            R.drawable.ic_sentiment_neutral,
+            R.drawable.ic_sentiment_satisfied,
+            R.drawable.ic_sentiment_very_satisfied
+    };
+
+    private int[] iconArrayInactive = {
+            R.drawable.ic_sentiment_very_dissatisfied_inactive,
+            R.drawable.ic_sentiment_dissatisfied_inactive,
+            R.drawable.ic_sentiment_neutral_inactive,
+            R.drawable.ic_sentiment_satisfied_inactive,
+            R.drawable.ic_sentiment_very_satisfied_inactive
+    };
+
+    private TextView msgview = null;
+
+    public SmileRatingBar(Context context) {
+        super(context);
+        init();
+    }
+
+    public SmileRatingBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SmileRatingBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        this.setMax(5);
+        this.setNumStars(5);
+        this.setStepSize(1.0f);
+        this.setRating(1.0f);
+    }
+
+    private Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = (DrawableCompat.wrap(drawable)).mutate();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+        int stars = getNumStars();
+        float rating = getRating();
+        float x = 0;
+
+        Bitmap bitmap;
+        Resources res = getResources();
+        Paint paint = new Paint();
+
+        int W = getWidth();
+        int H = getHeight();
+        int icon_size = (W / stars) - 32;//21 //(H < W)?(H):(W); //72
+        if (icon_size > H - 16) {
+            icon_size = H - 16;
+        }
+        int default_icon_size = icon_size;
+        int emoji_y_pos = (H / 2) - icon_size / 2;
+
+        int delta = ((H > W) ? (H) : (W)) / (stars);
+        int offset = (W - (icon_size + (stars - 1) * delta)) / 2;
+
+        for (int i = 0; i < stars; i++) {
+            if ((int) rating - 1 == i) {
+                bitmap = getBitmapFromVectorDrawable(getContext(), iconArrayActive[i]);
+                icon_size = default_icon_size+10 ;
+            } else {
+                bitmap = getBitmapFromVectorDrawable(getContext(), iconArrayInactive[i]);
+                icon_size = default_icon_size;
+            }
+            x = offset + (i * delta);
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, icon_size, icon_size, true);
+            canvas.drawBitmap(scaled, x, emoji_y_pos, paint);
+            canvas.save();
+        }
+    }
+
+
+}
